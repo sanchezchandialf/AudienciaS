e29dfb7c19467e95a0298eab1710546e1d78088b
sanchezchandialf <lautarosanche@gmail.com>
2025-02-04 11:51:45 -0300

    agregue comentarios

--- src/features/users/estadistic.tsx
+++ src/features/users/estadistic.tsx
@@ -6,16 +6,16 @@ const Estadistic: React.FC = () => {
     const [userName, setUserName] = useState<string>('');
     //Es necesario Parsear el json para poder obtener los datos 
     useEffect(() => {
-        // 1. Corregir la clave a 'user' (minúscula)
+    
         const storedUser = localStorage.getItem('user');
         
         if (storedUser) {
-            // 2. Parsear el JSON almacenado
+          
             const userObj = JSON.parse(storedUser);
             
-            // 3. Acceder a la propiedad correcta (ajusta 'nombre' según tu modelo de datos)
+           
             setUserName(userObj.nombre); 
-            // Si la propiedad se llama 'name' en tu UserProfile, usa userObj.name
+          
         }
     }, []);
 
